be8f62cfe45d90e43eb390f94dac12341df8fad2
chepeortiz2629 <jortizh@uninorte.edu.co>
2022-09-21 18:59:53 -0500

    PROYECTO

--- Gestion_notas_G11/app.py
+++ Gestion_notas_G11/app.py
@@ -6,6 +6,7 @@ app=Flask(__name__)
 def index():
     return render_template('login.html')
 
+
 @app.route('/login')
 def login():
     return render_template('login.html')
@@ -14,9 +15,36 @@ def login():
 def menu():
     return render_template('menu.html')
 
+
 @app.route('/usuarios')
 def menu_user():
     return render_template('usuarios.html')
 
+@app.route('/materias')
+def menu_materias():
+    return render_template('materias.html')
+
+
+@app.route('/cursos')
+def menu_cursos():
+    return render_template('cursos.html')
+
+
+@app.route('/matriculas')
+def menu_matriculas():
+    return render_template('matriculas.html')
+
+
+
+@app.route('/actividades')
+def menu_actividades():
+    return render_template('actividades.html')
+
+
+
+@app.route('/calificaciones')
+def menu_calificaciones():
+    return render_template('calificaciones.html')
+
 if  __name__=='__main__':
-     app.run(debug=True)
+     app.run(debug=True)  
